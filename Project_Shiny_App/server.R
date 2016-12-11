d872f445447596fe7c2b691da387fb326fe3b964
ReedKing <rking34@bu.edu>
2016-12-11 09:11:53 -0500

    First stage/formatting, beginning to put together moving parts

--- Project_Shiny_App/server.R
+++ Project_Shiny_App/server.R
@@ -1,26 +1,13 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
-library(shiny)
-
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
+server = function(input, output) {
   
-})
+  # Authentication
+  consumer_key <- "eofYOEKWlkufkIMh85PuXYYkm"
+  consumer_secret <- "LCn43w0Jsk4dbXxn6WKKJTfiwZ8pn807hy6gkM3b3FR5Otq84U"
+  access_token <- "807917512415440896-aoOUbyZJFyaKHPTBEtsLPI9lel6sytD"
+  access_secret <- "2mPrTaM39pGdh3JAM97dUYO2HHh9PQRJ9Vvwe2kMP5bsm"
+  options(httr_oauth_cache = TRUE)
+  setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+  
+  # Query to Twitter
+  
+}
--- Project_Shiny_App/ui.R
+++ Project_Shiny_App/ui.R
@@ -1,33 +1,33 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(leaflet)
+library(twitteR)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
-  )
-))
+ui = fluidPage(
+  theme = "flatly.css",
+  titlePanel("Twitter and Geographical Sentiment Analysis"),
+  navbarPage(title = "Sections",
+             tabPanel("Description of Project",
+                      h1("Brief:"),
+                      p("This tab contains more detailed discussion of each of the various moving parts
+                        of this project than the code commentary on their respective tabs will contain, if at
+                        any point you find yourself thinking, \"What the hell is this?\", the answer is probably here."),
+                      br(),
+                      h3("Data Import and Cleaning")
+             ),
+             tabPanel("Data Import", 
+                      fluidRow(
+                      column(4, textInput("searchkw", label = "Search For:", value = "Donald Trump"),
+                      column(4, textInput("lat", label = "Latitude:", value = 42.36)),
+                      column(4, textInput("long", label = "Longitude:", value = -71.06))),
+                      p("Here you can define your search term and location, it will work
+                        on any term in any location where it can find tweets, but I've provided
+                        the Latitude and Longitude of a couple major cities."),
+                      br(),
+                      p("cities here"))),
+             tabPanel("Mapping"),
+             tabPanel("Other Visual Explorations"),
+             tabPanel("Sentiment Analysis")
+             ))
+
+
+
